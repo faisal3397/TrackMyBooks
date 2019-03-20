5aa458d7d10af1cb2c87ea8011a84f9f3dfab90c
faisal3397 <faisal.alowaisi.97@gmail.com>
2019-03-20 22:29:51 +0300

    what shall we cook

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,39 @@
 $(function(){
-    mainFunction();
+
+    $.get(`https://www.themealdb.com/api/json/v1/1/categories.php`,function(res){
+        // let result = JSON.parse(res)
+        console.log(res.categories);
+        res.categories.forEach(el => {
+            console.log(el);
+            $("#categories").append(`
+            <div class="col-md-4 ">
+                <div class="card ">
+                    <img class="card-img-top" src="${el.strCategoryThumb}" alt="Recipe Thumbnail">
+                    <div class="card-body" style="width: 18rem;">
+                        <h5 class="card-title">${el.strCategory}</h5>
+                        <p class="card-text">${el.strCategoryDescription}</p>
+                        <a href="#" class="btn btn-primary">Go To Category</a>
+                    </div>
+                </div>
+            </div>
+        `)
+        })
+    })
+
+
+
+    // $("#searchResults").removeAttr('id');
 })
 
 function mainFunction(){
     console.log("It Works!!!");
     
-    $.get("http://openlibrary.org/search.json?q=the+lord+of+the+rings",function(res){
-        console.log(res.docs);
-        
-    })
+    
+}
+
+function searchResult(){
+   var input =  $("#search").val();
+   console.log(input);
+
+
 }
